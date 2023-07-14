c3a15512c7d4437df1fe131630d14f096e91b5df
Hiroki Saito <hiroki.saito@noppo190.com>
2023-07-15 01:09:09 +0900

    Receive POST request, create response using some data from the request

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,17 +1,49 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type UpdateProfileRequest struct {
+	Email string `json:"email"`
+}
 
 func main() {
 	// Initialize gin default instance
-	r := gin.Default()
+	router := gin.Default()
 
 	// Register a route
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
 	})
 
-	r.Run() // listen and serve on
+	router.POST("/user/profile/:name", updateProfile)
+
+	router.Run() // listen and serve on
+}
+
+func updateProfile(context *gin.Context) {
+	var request UpdateProfileRequest
+
+	if err := context.ShouldBindJSON(&request); err != nil {
+		context.JSON(
+			http.StatusBadRequest,
+			gin.H{
+				"error": err.Error(),
+			},
+		)
+		return
+	}
+
+	context.JSON(
+		http.StatusOK,
+		gin.H{
+			"name":  context.Param("name"),
+			"email": request.Email,
+		},
+	)
 }
